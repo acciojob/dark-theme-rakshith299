52d5ae4cc724a2fc91f78fa4be9205125782a720
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-16 18:24:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here
+function swapTheme(){
+    let main = document.getElementById("app");
+    let button = document.getElementById("swap");
+
+    console.log(main.classList.value);
+
+    if(main.classList.value === 'day'){
+        main.classList.remove('day');
+        main.classList.add('night');
+        button.classList.remove('button_day');
+        button.classList.add('button_night');
+    }else if(main.classList.value === 'night'){
+        main.classList.remove('night');
+        main.classList.add('day');
+        button.classList.remove('button_night');
+        button.classList.add('button_day');
+    }
+
+
+
+}
+
 
